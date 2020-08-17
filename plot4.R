9cd702b4911ad08bd3d9438bfb5ff3e11a5d5aaf
RoelGeorge <roelgeorge@gmail.com>
2020-08-18 00:25:40 +0530

    Reading and cleaning data for plot4

---
+++ plot4.R
@@ -0,0 +1,12 @@
+fn <- "household_power_consumption.txt"
+
+colNames = c("date", "time", "globalActivePower", "globalReactivePower",
+             "voltage", "globalIntensity", "subMetering1", "subMetering2",
+             "subMetering3")
+colClasses = c("character", "character", rep("numeric",7) )
+
+df <- read.table(fn, header=TRUE, sep=";", 
+                 col.names=colNames, colClasses=colClasses, na.strings="?")
+
+df$date = as.Date(df$date, format="%d/%m/%Y")
+df = df[df$date >= as.Date("2007-02-01") & df$date<=as.Date("2007-02-02"),]
