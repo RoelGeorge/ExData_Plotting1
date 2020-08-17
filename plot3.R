fb96d2d0124a0804c027e1479045749019c251d5
RoelGeorge <roelgeorge@gmail.com>
2020-08-18 00:19:51 +0530

    reading the data cleaning the data(date variable) creating the png file plot3.png

---
+++ plot3.R
@@ -0,0 +1,14 @@
+fn <- "household_power_consumption.txt"
+
+colNames = c("date", "time", "globalActivePower", "globalReactivePower",
+             "voltage", "globalIntensity", "subMetering1", "subMetering2",
+             "subMetering3")
+colClasses = c("character", "character", rep("numeric",7) )
+
+df <- read.table(fn, header=TRUE, sep=";", 
+                 col.names=colNames, colClasses=colClasses, na.strings="?")
+
+df$date = as.Date(df$date, format="%d/%m/%Y")
+df = df[df$date >= as.Date("2007-02-01") & df$date<=as.Date("2007-02-02"),]
+
+png(filename="plot2.png", width=480, height=480, units="px")
