fce16eaa02acac3bfca13bdf7462bdc4ee481fbd
RoelGeorge <roelgeorge@gmail.com>
2020-08-18 00:22:51 +0530

    adding plot to plot3.png

--- plot3.R
+++ plot3.R
@@ -11,4 +11,13 @@ df <- read.table(fn, header=TRUE, sep=";",
 df$date = as.Date(df$date, format="%d/%m/%Y")
 df = df[df$date >= as.Date("2007-02-01") & df$date<=as.Date("2007-02-02"),]
 
-png(filename="plot2.png", width=480, height=480, units="px")
+png(filename="plot3.png", width=480, height=480, units="px")
+
+with(df, {
+    plot(subMetering1,type="l", xaxt="n", xlab="", ylab="Energy sub metering")
+    lines(x=subMetering2, col="red")
+    lines(x=subMetering3, col="blue")
+})
+axis(1, at=c(1, as.integer(nrow(df)/2), nrow(df)), labels=c("Thu", "Fri", "Sat"))
+legend("topright", lty=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
