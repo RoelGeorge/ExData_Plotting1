6f211eac8a8aaa4a87d6d31d70fedf477d0132ee
RoelGeorge <roelgeorge@gmail.com>
2020-08-18 00:39:07 +0530

    adding comments

--- plot3.R
+++ plot3.R
@@ -1,18 +1,20 @@
+## Assigning values to vriables 
 fn <- "household_power_consumption.txt"
 
 colNames = c("date", "time", "globalActivePower", "globalReactivePower",
              "voltage", "globalIntensity", "subMetering1", "subMetering2",
              "subMetering3")
 colClasses = c("character", "character", rep("numeric",7) )
-
+## reading the data using the variables 
 df <- read.table(fn, header=TRUE, sep=";", 
                  col.names=colNames, colClasses=colClasses, na.strings="?")
-
+## changing class of df$date and formatting it correctly
 df$date = as.Date(df$date, format="%d/%m/%Y")
 df = df[df$date >= as.Date("2007-02-01") & df$date<=as.Date("2007-02-02"),]
+## creating plot3.png
 
 png(filename="plot3.png", width=480, height=480, units="px")
-
+## Plotting plot3.png
 with(df, {
     plot(subMetering1,type="l", xaxt="n", xlab="", ylab="Energy sub metering")
     lines(x=subMetering2, col="red")
